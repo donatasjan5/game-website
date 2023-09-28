2d90377248f633acd81bfa3ecb7367d510a794eb
donatasjan5 <donatasjan5@gmail.com>
2023-09-28 17:04:46 +0300

    more css to the cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.css";
 import GameSwiper from "../components/GameSwiper";
+import GameCard from "../components/GameCard";
 
 const Home = ({ games }) => {
   return (
@@ -9,16 +10,23 @@ const Home = ({ games }) => {
         <div className="row">
           <GameSwiper games={games} />
         </div>
-        <div className="row">
+        <div className="row mb-4 mt-4">
           <div className="col-lg-6">
             <h2 className="sectionTitle">Games on promotion</h2>
           </div>
-          <div className="col-lg-6">
-<a href="#" className="viewMore">
-  View More Games
-<i class="bi bi-arrow-right"></i>
-</a>
-</div>
+          <div className="col-lg-6 d-flex justify-content-end align-items-center">
+            <a href="#" className="viewMore">
+              View More Games
+              <i class="bi bi-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          {
+            games.slice(0,4).map(game=>(
+              <GameCard key={game.id} game={game} />
+            ))
+          }
         </div>
       </div>
     </section>
