2695d7e3ef84f158e8db208891bb98d9ef75ffe7
donatasjan5 <donatasjan5@gmail.com>
2023-09-28 14:32:49 +0300

    code modularization

--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import GameSlide from "./GameSlide";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -10,19 +11,19 @@ import "./gameSwiper.css";
 import { EffectCoverflow, Navigation, Autoplay } from "swiper/modules";
 
 function GameSwiper({ games }) {
-    const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false);
 
-    const handleToggleVideo = () => {
-        setActive(!active);
-    }
-    
+  const handleToggleVideo = () => {
+    setActive(!active);
+  };
+
   return (
     <Swiper
       effect={"coverflow"}
       grabCursor={true}
       navigation={true}
       loop={true}
-      centeredslides={true}
+      centeredSlides={true}
       slidesPerView={"auto"}
       coverflowEffect={{
         rotate: 35,
@@ -40,26 +41,11 @@ function GameSwiper({ games }) {
     >
       {games.map((game) => (
         <SwiperSlide key={game.id}>
-          <div className="gameSlider">
-            <img src={game.img} alt="game Image" />
-            <div className="content">
-              <h2>{game.title}</h2>
-              <p>{game.description}</p>
-              <div className="buttons">
-                <a href="#" className="orderBtn">
-                  Order Now
-                </a>
-                <a href="#" className={`playBtn ${active ? 'active' : undefined}`} onClick={handleToggleVideo}>
-                  <span className="pause">
-                    <i className="bi bi-pause-fill"></i>
-                  </span>
-                  <span className="play">
-                    <i class="bi bi-play-fill"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div>
+        <GameSlide
+        game={game}
+        active={active}
+        toggleVideo={handleToggleVideo}
+        />
         </SwiperSlide>
       ))}
     </Swiper>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import './home.css';
-import GameSwiper from '../components/GameSwiper';
+import React from "react";
+import "./home.css";
+import GameSwiper from "../components/GameSwiper";
 
-const Home = ({games}) => {
+const Home = ({ games }) => {
   return (
-    <section id='home' className='home active'>
-        <div className='container-fluid'>
-<div className="row">
-    <GameSwiper games={games} />
-</div>
+    <section id="home" className="home active">
+      <div className="container-fluid">
+        <div className="row">
+          <GameSwiper games={games} />
         </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <h2 className="sectionTitle">Games on promotion</h2>
+          </div>
+          <div className="col-lg-6">
+            View More Games <i className="bi bi-arrow-right"></i>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
