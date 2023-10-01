d197ba58620c8e8dadab332203679d28cb7a2fa2
donatasjan5 <donatasjan5@gmail.com>
2023-10-01 21:31:20 +0300

    Categories filter

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./categories.css";
+import filterListData from "../data/filterListData";
 
-const Categories = ({games, reference}) => {
-  return;
-  <section id="categories" className="categories" ref={reference}>
-    <h1>categories</h1>
-  </section>;
+const Categories = ({ games, reference }) => {
+  const [filters, setFilters] = useState(filterListData);
+
+  const handleFilterGames = (category) => {
+   setFilters (
+    filters.map((filter) => {
+      filter.active = false;
+      if (filter.name === category) {
+        filter.active = true;
+      }
+
+      return filter;
+    })
+    );
+  };
+
+  return (
+    <section id="categories" className="categories" ref={reference}>
+      <div className="conatiner-fluid mt-2">
+        <div className="row">
+          <div className="col-lg-8">
+            <ul className="filters">
+              {filters.map((filter) => (
+                <li key={filter._id} className={`${filter.active ? 'active' : undefined}`} onClick={() => handleFilterGames(filter.name)}>{filter.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-lg-4">
+            .
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
